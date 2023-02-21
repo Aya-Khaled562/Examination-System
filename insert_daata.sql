f6c8fb009de9c9b809c02548ac12437a14d95866
Maha Elgendy <mahaelgende@gmail.com>
2023-02-22 00:07:14 +0200

    add data

--- insert_daata.sql
+++ insert_daata.sql
@@ -1,19 +1,26 @@
-INSERT INTO Student 
-       VALUES('Ahmed','elemam','m','01027219250','1999-02-13','2020-02-23','cairo','giza'),
-	         ('Ali','ali','m','01023219250','1990-04-19','2022-06-1','alex','khaledibnwalid'),
-			 ('Amged','mohamed','m','01027212250','1980-02-12','2015-05-30','Tanta','elnahas'),
-			 ('Ayman','belal','m','01027219250','2000-03-15','2017-08-02','cairo','giza'),
-			 ('Samir','Zakaria','m','01027213250','1993-04-14','2020-04-20','cairo','giza'),
-			 ('Mohamed','Hany','m','01027219253','1992-08-09','2020-04-21','cairo','giza'),
-			 ('Fares','Mohamed','m','01027219252','1995-02-05','2022-4-20','cairo','giza')
+INSERT INTO Student ([fname]
+      ,[lname]
+      ,[st_gender]
+      ,[st_phone]
+      ,[st_birthdate]
+      ,[st_joindate]
+      ,[dept_id]
+      ,[city]
+      ,[street])
+       VALUES('Ahmed','elemam','m','01027219250','1999-02-13','2020-02-23',1,'cairo','giza'),
+	         ('Ali','ali','m','01023219250','1990-04-19','2022-06-1',2,'alex','khaledibnwalid'),
+			 ('Amged','mohamed','m','01027212250','1980-02-12','2015-05-30',3,'Tanta','elnahas'),
+			 ('Ayman','belal','m','01027219250','2000-03-15','2017-08-02',4,'cairo','giza'),
+			 ('Samir','Zakaria','m','01027213250','1993-04-14','2020-04-20',5,'cairo','giza'),
+			 ('Mohamed','Hany','m','01027219253','1992-08-09','2020-04-21',1,'cairo','giza'),
+			 ('Fares','Mohamed','m','01027219252','1995-02-05','2022-4-20',2,'cairo','giza')
 
-INSERT INTO Department 
-		VALUES 
-		('SD',1,'2020-03-03'),
-		('PD',2,'2010-12-23'),
-		('OS',3,'2015-10-13'),
-		('JAVA',1,'2021-09-17'),
-		('AI',1,'2005-11-30')
+INSERT INTO Department(dept_name,dept_manager)
+		VALUES ('SD',1),
+		('PD',2),
+		('OS',3),
+		('JAVA',1),
+		('AI',1)
 
 INSERT INTO Course
 			VALUES ('C++',2,1),
@@ -23,13 +30,13 @@ INSERT INTO Course
 					('Nodejs',12,3),
 					('ASP.Net',3,3)
 
-INSERT INTO Instructor(ins_fname,ins_lname,Ins_phone,ins_city,ins_street,Dept_id)
-			VALUES ('Maha','Elgendy','01025689','Tanta','Ebn Elfared',1),
-					('Aya','Khaled','01265687','Kafr Elsheikh','Abass Elakaad',2),
-					('Mayar','Ahmed','01025689','Zefta','Moheb',3),
-					('Nahla','Adly','01025689','Kafr Elzayat','saeed',4),
-					('Abdelrahman','Emam','01025689','Tanta','Elnahas',5),
-					('Mai','Mohamed','01025689','Elmahala','Ebn Elfared',NULL)
+INSERT INTO Instructor([fname],[lname],[ins_phone],[dept_id],[city],[street])
+			VALUES ('Maha','Elgendy','01025689',NULL,'Tanta','Ebn Elfared'),
+					('Aya','Khaled','01265687',NULL,'Kafr Elsheikh','Abass Elakaad'),
+					('Mayar','Ahmed','01025689',NULL,'Zefta','Moheb'),
+					('Nahla','Adly','01025689',NULL,'Kafr Elzayat','saeed'),
+					('Abdelrahman','Emam','01025689',NULL,'Tanta','Elnahas'),
+					('Mai','Mohamed','01025689',NULL,'Elmahala','Ebn Elfared')
 
 INSERT INTO Topic
              VALUES('Programming'),
