3675075193f0ab12e71a93b9a655c3470265d11a
Maha Elgendy <mahaelgende@gmail.com>
2023-02-24 01:07:53 +0200

    add instructor courses Sp

--- select_SP.sql
+++ select_SP.sql
@@ -1,12 +1,59 @@
 
-CREATE PROC instr_crs_select
+--instructor--
+CREATE PROC instructor_select_sp
 AS
-BEGIN
-SELECT *FROM Instructor 
+	BEGIN
+	SELECT *FROM Instructor 
 END
 
-instr_crs_select
+instructor_select_sp
 
+GO
+
+CREATE PROC instructor_select_by_id_sp  @id int
+AS
+	BEGIN
+	SELECT *FROM Instructor 
+	WHERE insrt_id = @id
+END
+instructor_select_by_id_sp 1
+
+GO
+----------------------------------------------------------------------------------------------------
+--instructor Course --
+
+CREATE PROC instructor_course_select_sp
+AS
+	BEGIN
+	SELECT *FROM Instructor_courses  
+END
+
+instructor_course_select_sp
+
+GO
+
+CREATE PROC instructor_select_by_course_id_sp  @course_id int
+AS
+	BEGIN
+	SELECT *FROM Instructor_courses  
+	WHERE course_id = @course_id
+END
+
+instructor_select_by_course_id_sp 3
+
+GO
+
+CREATE PROC instructor_select_by_instructor_id_sp  @instructor_id int
+AS
+	BEGIN
+	SELECT *FROM Instructor_courses  
+	WHERE instr_id = @instructor_id
+END
+
+instructor_select_by_instructor_id_sp 1
+
+----------------------------------------------------------------------------------------------------
+-- Course --
 CREATE PROC coures_select_sp
 AS
 BEGIN
@@ -15,32 +62,37 @@ BEGIN
 
 coures_select_sp
 
+GO
+CREATE PROC coures_select_by_id @id int
+AS
+	BEGIN
+	   SELECT * FROM Course c
+	   WHERE c.courses_id = @id
+END
 
+coures_select_by_id 2
+
+-----------------------------------------------------------------------------------------------
+-- Student Courses --
 CREATE PROC std_cs_select_sp
 AS
-BEGIN
-SELECT* FROM
-Student_course sc
+	BEGIN
+	SELECT* FROM
+	Student_course sc
 END
-
 std_cs_select_sp
 
+GO
 
-CREATE PROC coures_select @id int
+CREATE PROC std_cs_select_by_id @id int
 AS
-BEGIN
-   SELECT * FROM Course c
-   WHERE c.courses_id = @id
-   END
-
-coures_select 2
-
-CREATE PROC std_cs_select @id int
-AS
-BEGIN
-SELECT* FROM
-Student_course sc
-WHERE sc.st_id = @id
+	BEGIN
+	SELECT* FROM
+	Student_course sc
+	WHERE sc.st_id = @id
 END
 
-std_cs_select 3
+std_cs_select_by_id 3
+
+GO
+-----------------------------------------------------------------------------------
