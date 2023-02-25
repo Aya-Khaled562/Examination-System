118a50d319472261f1c1cc5e027b17da2a046d04
Maha Elgendy <mahaelgende@gmail.com>
2023-02-25 23:01:06 +0200

    update exam correction

--- Stored Procedures/Exam Stored Procedure/exam_correction.sql
+++ Stored Procedures/Exam Stored Procedure/exam_correction.sql
@@ -28,21 +28,30 @@ END
 
 GO
 
-CREATE PROCEDURE correct_exam @st_id INT, @exam_id INT, @total_grade INT OUT
+CREATE PROCEDURE correct_exam @st_id INT, @exam_id INT
 AS
 BEGIN
-  DECLARE @grade INT;
   
-  EXEC get_student_answers @st_id,@exam_id;
+	IF (select top(1) is_corrected from Student_exams_questions where exam_id=@exam_id and st_id=@st_id)=0
+	BEGIN
+		  EXEC get_student_answers @st_id,@exam_id;
   
-  UPDATE dbo.Student_exams_questions 
-  SET Student_exams_questions.grade =CASE WHEN seq.answer = q.q_answer THEN q.q_grade ELSE 0 END
-  FROM dbo.Student_exams_questions seq
-  JOIN question q ON seq.qs_id = q.q_id
-  WHERE seq.st_id = @st_id AND seq.exam_id =@exam_id;
+		  UPDATE dbo.Student_exams_questions 
+		  SET Student_exams_questions.grade =CASE WHEN seq.answer = q.q_answer THEN q.q_grade ELSE 0 END
+		  FROM dbo.Student_exams_questions seq
+		  JOIN question q ON seq.qs_id = q.q_id
+		  WHERE seq.st_id = @st_id AND seq.exam_id =@exam_id;
 
-  exec ExamCorrection_calc_std_garde_sp @st_id,@exam_id
+		  update dbo.Student_exams_questions
+		  SET is_corrected=1
+		  WHERE exam_id=@exam_id and st_id=@st_id
+		
+		
+		  exec ExamCorrection_calc_std_garde_sp @st_id,@exam_id
+		  PRINT 'exam for this student is corrected'
+	END
+	ELSE
+		PRINT 'exam for this student is already corrected'
 END
- 
- GO
 
+correct_exam 2,1
