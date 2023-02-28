0dea131dd7be40e6449ec90bf4eff7d286aef82b
Maha Elgendy <mahaelgende@gmail.com>
2023-02-28 16:35:26 +0200

    sp

--- Stored Procedures/Stored Procedure For Tables/insert_SP.sql
+++ Stored Procedures/Stored Procedure For Tables/insert_SP.sql
@@ -1,18 +1,18 @@
----- instructor --
-create procedure instructor_insert_sp  @ins_fname nvarchar(50),@ins_lname nvarchar(50),@dept_id int
-as 
-begin
-	if @dept_id in (select  dept_id from Department)
-		 begin
-		
-				insert into Instructor (fname,lname,dept_id)
-				values(@ins_fname,@ins_lname,@dept_id)
-		end
-	else 
-		begin
-		print 'department_id you try to enter is not exist'
-	end
-end;
+---- instructor --------------------
+CREATE PROCEDURE instructor_insert_sp  @ins_fname NVARCHAR(50),@ins_lname NVARCHAR(50),@dept_id INT
+AS 
+BEGIN
+	IF @dept_id IN (SELECT  dept_id FROM Department)
+		 BEGIN
+
+				INSERT INTO Instructor (fname,lname,dept_id)
+				VALUES(@ins_fname,@ins_lname,@dept_id)
+		END
+	ELSE 
+		BEGIN
+		PRINT 'department_id you try to enter is not exist'
+	END
+END;
 
 GO
 instructor_insert_sp 'Aya','Ebrahim',2
@@ -40,7 +40,7 @@ instructor_course_insert_sp 2,3
 GO
 ---------------------------------------
 --Courses
-CREATE PROC insert_courese_sp   @courses_name VARCHAR(50), @courser_durtion INT ,@topic_id INT
+CREATE PROC insert_courese_sp  @courses_name VARCHAR(50), @courser_durtion INT ,@topic_id INT
 AS
 BEGIN
 IF @topic_id IN (SELECT t.topic_id FROM Topic t)
@@ -54,17 +54,18 @@ ELSE
 	 END
 END
 
+GO
 insert_courese_sp 'IOS',40,3
-
+Go
 ---------------------------------
 ---Student Courese
-CREATE PROC st_insert_pro @courese_id INT , @st_id INT ,@grade INT
+CREATE PROC student_Course_insert_sp @course_id INT , @st_id INT ,@grade INT
 AS
 BEGIN
-IF @courese_id IN (SELECT c.courses_id FROM Course c) AND @st_id IN (SELECT s.st_id FROM Student s)
+IF @course_id IN (SELECT c.courses_id FROM Course c) AND @st_id IN (SELECT s.st_id FROM Student s)
   BEGIN
     INSERT INTO Student_course
-	VALUES(@courese_id,@st_id,@grade)
+	VALUES(@course_id,@st_id,@grade)
   END
 ELSE
    BEGIN
@@ -72,9 +73,68 @@ ELSE
    END
 END
 
-st_insert_pro 3,3,50
+GO
+student_Course_insert_sp 3,2,50
+GO
 
----------- Mayar -----------
+-----------------exam-------------
+
+CREATE PROCEDURE exam_insert @ex_date DATE
+AS 
+BEGIN
+	INSERT INTO Exam(ex_date) 
+	VALUES (@ex_date)
+END
+
+GO
+EXECUTE exam_insert '2023-2-11'
+GO
+
+-------------student_exams_Questions -----------------
+CREATE PROCEDURE st_ex_qs_insert @st_id INT , @ex_id INT ,@qs_id INT, @grade INT,@answer VARCHAR(10),@is_corrected INT 
+AS
+BEGIN
+	IF 
+		(@st_id IN (SELECT st_id FROM Student)) 
+		AND 
+		(@ex_id IN (SELECT exam_id FROM Exam ))
+		AND 
+		(@qs_id IN (SELECT q_id FROM Question ))
+		BEGIN
+			IF @st_id IN (SELECT st_id FROM  Student_exams_questions 
+			WHERE st_id =@st_id AND exam_id = @ex_id AND qs_id = @qs_id )
+			BEGIN
+				PRINT 'dublicatec student_id , exam_id and qs_id'
+			END
+		ELSE
+		BEGIN
+			INSERT INTO student_exams_Questions(st_id,exam_id,qs_id,grade,answer,is_corrected)
+			VALUES (@st_id,@ex_id,@qs_id,@grade,@answer,@is_corrected)
+		END
+		END
+	ELSE 
+		BEGIN
+			PRINT 'this student_Exam_question not exist'
+		END
+END
+
+GO
+EXECUTE st_ex_qs_insert 3 ,1 ,7 ,2, 'text',1
+
+GO
+--------------Topic  -------------------
+
+CREATE PROCEDURE topic_insert @topic_name VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO topic (topic_name)
+	VALUES(@topic_name)
+END
+
+EXECUTE topic_insert 'frontend'
+
+GO
+-------------------------------- Questions --------------------
 CREATE PROCEDURE insert_question @question VARCHAR(50), @q_type VARCHAR(10), @grade INT, @answer VARCHAR(3), @c_id INT 
 AS 
 BEGIN
@@ -88,8 +148,11 @@ BEGIN
 			PRINT 'course_id you tried to enter is not exist'
 		END
 END
+GO
 insert_question 'Identify the scope resolution operator.','choice',2,'b',2
+GO
 
+---------------------------Question Choices---------------
 CREATE PROCEDURE insert_choices @q_id INT, @choice VARCHAR(30) 
 AS 
 BEGIN
@@ -103,17 +166,8 @@ BEGIN
 			PRINT 'Question_id you tried to enter is not exist'
 		END
 END
-insert_choices 7, ':'
-insert_choices 7, '::'
-insert_choices 7, '?:'
-insert_choices 7, 'None'
 
-
-<<<<<<< HEAD:insert_SP.sql
-=======
 GO
-
-
 ---------------------------Student--------------------------
 CREATE PROC student_insert_sp @fname VARCHAR(50) , @lname VARCHAR(50) ,@st_gender VARCHAR(3), @st_phone VARCHAR(11),
 @st_birthdate DATE , @st_joindate DATE ,@dept_id INT, @city VARCHAR(50),@street VARCHAR(50)
@@ -150,4 +204,3 @@ END
 GO
 department_insert_sp 'test',2
 
->>>>>>> 2aae800c6a062ec8f60bff445117ffefd4aaa8c3:Stored Procedures/Stored Procedure For Tables/insert_SP.sql
